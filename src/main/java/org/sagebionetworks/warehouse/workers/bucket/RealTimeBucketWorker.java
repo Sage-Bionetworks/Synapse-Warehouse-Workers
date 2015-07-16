dac32232b1e1e1251dd14a1ba0d5ee78c5e46e1b
john-hill <john.hill@sagebase.org>
2015-07-15 17:15:13 -0700

    pre-merge

--- src/main/java/org/sagebionetworks/warehouse/workers/bucket/RealTimeBucketWorker.java
+++ src/main/java/org/sagebionetworks/warehouse/workers/bucket/RealTimeBucketWorker.java
@@ -1,19 +1,30 @@
 package org.sagebionetworks.warehouse.workers.bucket;
 
+import org.sagebionetworks.warehouse.workers.db.FileManager;
 import org.sagebionetworks.workers.util.aws.message.MessageDrivenRunner;
 import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
 import org.sagebionetworks.workers.util.progress.ProgressCallback;
 
 import com.amazonaws.services.sqs.model.Message;
+import com.google.inject.Inject;
 
 public class RealTimeBucketWorker implements MessageDrivenRunner {
+	
+	private FileManager fileManager;
+	
+	@Inject
+	public RealTimeBucketWorker(FileManager fileManager) {
+		super();
+		this.fileManager = fileManager;
+	}
 
 
 	@Override
 	public void run(ProgressCallback<Message> progressCallback, Message message)
 			throws RecoverableMessageException, Exception {
 		
-		System.out.println(message.getBody());
+		// let the manger know about the file
+		
 
 	}
 
