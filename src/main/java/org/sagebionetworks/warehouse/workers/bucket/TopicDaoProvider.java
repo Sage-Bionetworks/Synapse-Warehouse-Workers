4038aff7ab0558c1b37769bde6f399b25e26728e
Kimyen <kimyen0816@gmail.com>
2015-07-30 18:45:10 -0700

    correct doc

--- src/main/java/org/sagebionetworks/warehouse/workers/bucket/TopicDaoProvider.java
+++ src/main/java/org/sagebionetworks/warehouse/workers/bucket/TopicDaoProvider.java
@@ -1,17 +1,17 @@
 package org.sagebionetworks.warehouse.workers.bucket;
 
 /**
- * This provider creates topics for each snapshot record and returns the topicArn.
+ * This provider creates topics for each snapshot record type and returns the topicArn.
  */
 public interface TopicDaoProvider {
 
 	/**
-	 * Get the topicArn for the given topicName
+	 * Get the topicArn for the given snapshot record type
 	 * 
-	 * @param topicName
+	 * @param type
 	 * @effect create the topic if it does not exist
 	 * @return
 	 */
-	public String getTopicArn(String topicName);
+	public String getTopicArn(String type);
 
 }
