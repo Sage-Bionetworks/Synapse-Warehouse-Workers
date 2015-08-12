2f83e1d836a9ddd554f9a1c7e563a28a18b1477f
john-hill <john.hill@sagebase.org>
2015-08-11 18:21:35 -0700

    forgot new classess

---
+++ src/main/java/org/sagebionetworks/warehouse/workers/SemaphoreKey.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.warehouse.workers;
+
+/**
+ * The enumeration of semaphore keys used by this application.
+ *
+ */
+public enum SemaphoreKey {
+	
+	BUCKET_SCANNING_WORKER,
+	REALTIME_BUCKET_LISTENER_WORKER,
+	PERIODIC_ROLLING_FOLDER_MESSAGE_GENERATOR,
+	FOLDER_COLLATE_WORKER,
+}
---
+++ src/main/java/org/sagebionetworks/warehouse/workers/collate/PeriodicRollingFolderStack.java
@@ -0,0 +1,47 @@
+package org.sagebionetworks.warehouse.workers.collate;
+
+import org.sagebionetworks.database.semaphore.CountingSemaphore;
+import org.sagebionetworks.warehouse.workers.SemaphoreKey;
+import org.sagebionetworks.warehouse.workers.WorkerStackConfiguration;
+import org.sagebionetworks.warehouse.workers.WorkerStackConfigurationProvider;
+import org.sagebionetworks.workers.util.semaphore.SemaphoreGatedWorkerStack;
+import org.sagebionetworks.workers.util.semaphore.SemaphoreGatedWorkerStackConfiguration;
+
+import com.google.inject.Inject;
+
+/**
+ * This stack runs as a singleton on a timer. The stack will find all folders
+ * with a state of 'rolling' For each folder found a message will be pushed to
+ * the collate worker queue.
+ * 
+ * @author John
+ * 
+ */
+public class PeriodicRollingFolderStack implements
+		WorkerStackConfigurationProvider {
+
+	WorkerStackConfiguration config;
+	
+	@Inject
+	public PeriodicRollingFolderStack(CountingSemaphore semaphore, PeriodicRollingFolderMessageGeneratorWorker worker){
+		SemaphoreGatedWorkerStackConfiguration config = new SemaphoreGatedWorkerStackConfiguration();
+		config.setProgressingRunner(worker);
+		config.setSemaphoreLockKey(SemaphoreKey.PERIODIC_ROLLING_FOLDER_MESSAGE_GENERATOR.name());
+		config.setSemaphoreLockTimeoutSec(30);
+		// This is singleton.
+		config.setSemaphoreMaxLockCount(1);
+		Runnable mainRunner = new SemaphoreGatedWorkerStack(semaphore, config);
+		this.config = new WorkerStackConfiguration();
+		this.config.setRunner(mainRunner);
+		this.config.setStartDelayMs(3);
+		// This worker only runs every 20 mins.
+		this.config.setPeriodMS(60*1000*20);
+		this.config.setWorkerName(PeriodicRollingFolderStack.class.getName());
+	}
+
+	@Override
+	public WorkerStackConfiguration getWorkerConfiguration() {
+		return config;
+	}
+
+}
