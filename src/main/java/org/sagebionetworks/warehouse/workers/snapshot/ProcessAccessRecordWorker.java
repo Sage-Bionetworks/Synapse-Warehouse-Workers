0328e6aa035a25d5c40072bf7ed7a64013118d14
Kimyen <kimyen0816@gmail.com>
2015-08-14 12:15:00 -0700

    delete file and close reader

--- src/main/java/org/sagebionetworks/warehouse/workers/snapshot/ProcessAccessRecordWorker.java
+++ src/main/java/org/sagebionetworks/warehouse/workers/snapshot/ProcessAccessRecordWorker.java
@@ -1,20 +1,29 @@
 package org.sagebionetworks.warehouse.workers.snapshot;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.sagebionetworks.aws.utils.s3.ObjectCSVReader;
+import org.sagebionetworks.aws.utils.sns.MessageUtil;
 import org.sagebionetworks.repo.model.audit.AccessRecord;
+import org.sagebionetworks.warehouse.workers.bucket.FileSubmissionMessage;
 import org.sagebionetworks.warehouse.workers.db.ProcessedAccessRecordDao;
 import org.sagebionetworks.warehouse.workers.model.ProcessedAccessRecord;
 import org.sagebionetworks.warehouse.workers.utils.AccessRecordUtils;
+import org.sagebionetworks.warehouse.workers.utils.XMLUtils;
 import org.sagebionetworks.workers.util.aws.message.MessageDrivenRunner;
 import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
 import org.sagebionetworks.workers.util.progress.ProgressCallback;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.sqs.model.Message;
 import com.google.inject.Inject;
 
@@ -37,26 +46,42 @@ public class ProcessAccessRecordWorker implements MessageDrivenRunner {
 
 	@Override
 	public void run(ProgressCallback<Message> callback, Message message)
-			throws RecoverableMessageException, Exception {
-		ObjectCSVReader<AccessRecord> reader = AccessRecordWorkerUtil.getAccessRecordReader(message, s3Client);
-		AccessRecord record = reader.next();
-		List<ProcessedAccessRecord> batch = new ArrayList<ProcessedAccessRecord>();
+			throws RecoverableMessageException, IOException {
+		// extract the bucket and key from the message
+		String xml = MessageUtil.extractMessageBodyAsString(message);
+		FileSubmissionMessage fileSubmissionMessage = XMLUtils.fromXML(xml, FileSubmissionMessage.class, "Message");
 
-		while (record != null) {
-			if (!AccessRecordUtils.isValidAccessRecord(record)) {
-				log.error("Invalid Access Record: " + record.toString());
-				continue;
+		// read the file as a stream
+		File file = null;
+		try {
+			File.createTempFile("collatedAccessRecords", ".csv.gz");
+			s3Client.getObject(new GetObjectRequest(fileSubmissionMessage.getBucket(), fileSubmissionMessage.getKey()), file);
+			ObjectCSVReader<AccessRecord> reader = new ObjectCSVReader<AccessRecord>(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)), "UTF-8"), AccessRecord.class);
+
+			AccessRecord record = reader.next();
+			List<ProcessedAccessRecord> batch = new ArrayList<ProcessedAccessRecord>();
+	
+			while (record != null) {
+				if (!AccessRecordUtils.isValidAccessRecord(record)) {
+					log.error("Invalid Access Record: " + record.toString());
+					continue;
+				}
+				batch.add(AccessRecordUtils.processAccessRecord(record));
+				if (batch.size() == 1000) {
+					dao.insert(batch);
+					batch = new ArrayList<ProcessedAccessRecord>();
+				}
+				record = reader.next();
 			}
-			batch.add(AccessRecordUtils.processAccessRecord(record));
-			if (batch.size() == 1000) {
+	
+			if (batch.size() > 0) {
 				dao.insert(batch);
-				batch = new ArrayList<ProcessedAccessRecord>();
 			}
-			record = reader.next();
-		}
 
-		if (batch.size() > 0) {
-			dao.insert(batch);
+			reader.close();
+		} finally {
+			if (file != null)
+				file.delete();
 		}
 	}
 
