a591ab22ff96b9a202073f50561e82a091f59731
Kimyen <kimyen0816@gmail.com>
2015-08-25 19:58:35 -0700

    AclSnapshotDao

--- src/main/java/org/sagebionetworks/warehouse/workers/db/AclSnapshotDaoImpl.java
+++ src/main/java/org/sagebionetworks/warehouse/workers/db/AclSnapshotDaoImpl.java
@@ -1,28 +1,32 @@
 package org.sagebionetworks.warehouse.workers.db;
 
-import static org.sagebionetworks.warehouse.workers.db.Sql.COL_ACL_SNAPSHOT_CREATED_ON;
-import static org.sagebionetworks.warehouse.workers.db.Sql.COL_ACL_SNAPSHOT_OWNER_ID;
-import static org.sagebionetworks.warehouse.workers.db.Sql.COL_ACL_SNAPSHOT_OWNER_TYPE;
-import static org.sagebionetworks.warehouse.workers.db.Sql.COL_ACL_SNAPSHOT_TIMESTAMP;
-import static org.sagebionetworks.warehouse.workers.db.Sql.TABLE_ACL_SNAPSHOT;
+import static org.sagebionetworks.warehouse.workers.db.Sql.*;
 
+import java.sql.Blob;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.sagebionetworks.repo.model.ObjectType;
+import org.sagebionetworks.repo.model.ResourceAccess;
 import org.sagebionetworks.warehouse.workers.model.AclSnapshot;
 import org.sagebionetworks.warehouse.workers.utils.ClasspathUtils;
+import org.sagebionetworks.warehouse.workers.utils.XMLUtils;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.google.inject.Inject;
+import com.thoughtworks.xstream.io.StreamException;
 
 public class AclSnapshotDaoImpl implements AclSnapshotDao{
 
+	private static final String EMPTY_SET_XML = "<set\\>";
 	private static final String TRUNCATE = "TRUNCATE TABLE " + TABLE_ACL_SNAPSHOT;
 	private static final String INSERT_IGNORE = "INSERT IGNORE INTO "
 			+ TABLE_ACL_SNAPSHOT
@@ -34,7 +38,9 @@ public class AclSnapshotDaoImpl implements AclSnapshotDao{
 			+ COL_ACL_SNAPSHOT_OWNER_TYPE
 			+ ","
 			+ COL_ACL_SNAPSHOT_CREATED_ON
-			+ ") VALUES (?,?,?,?)";
+			+ ","
+			+ COL_ACL_SNAPSHOT_RESOURCE_ACCESS
+			+ ") VALUES (?,?,?,?,?)";
 
 	private static final String ACL_SNAPSHOT_DDL_SQL = "AclSnapshot.ddl.sql";
 	private static final String SQL_GET = "SELECT * FROM "
@@ -46,6 +52,7 @@ public class AclSnapshotDaoImpl implements AclSnapshotDao{
 			+ " = ? AND "
 			+ COL_ACL_SNAPSHOT_OWNER_TYPE
 			+ " = ?";
+	protected static final String RESOURCE_ACCESS_ALIAS = "ResourceAccessSet";
 
 	private JdbcTemplate template;
 
@@ -57,11 +64,25 @@ public class AclSnapshotDaoImpl implements AclSnapshotDao{
 		public AclSnapshot mapRow(ResultSet rs, int arg1) throws SQLException {
 			AclSnapshot snapshot = new AclSnapshot();
 			snapshot.setTimestamp(rs.getLong(COL_ACL_SNAPSHOT_TIMESTAMP));
-			snapshot.setOwnerId(rs.getLong(COL_ACL_SNAPSHOT_OWNER_ID));
+			snapshot.setId("" + rs.getLong(COL_ACL_SNAPSHOT_OWNER_ID));
 			snapshot.setOwnerType(ObjectType.valueOf(rs.getString(COL_ACL_SNAPSHOT_OWNER_TYPE)));
 			Long createdOn = rs.getLong(COL_ACL_SNAPSHOT_CREATED_ON);
 			if (!rs.wasNull()) {
-				snapshot.setCreatedOn(createdOn);
+				snapshot.setCreationDate(new Date(createdOn));
+			}
+			Blob resourceAccessBlob = rs.getBlob(COL_ACL_SNAPSHOT_RESOURCE_ACCESS);
+			if (resourceAccessBlob != null) {
+				String resourceAccess = new String(resourceAccessBlob.getBytes(1, (int) resourceAccessBlob.length()));
+				try {
+					snapshot.setResourceAccess(XMLUtils.fromXML(resourceAccess, Set.class, RESOURCE_ACCESS_ALIAS));
+				} catch (StreamException e) {
+					if (e.getMessage().contains("input contained no data")) {
+						if (resourceAccess.contains(EMPTY_SET_XML)) 
+							snapshot.setResourceAccess(new HashSet<ResourceAccess>());
+					} else {
+						throw new RuntimeException();
+					}
+				}
 			}
 			return snapshot;
 		}
@@ -88,12 +109,20 @@ public class AclSnapshotDaoImpl implements AclSnapshotDao{
 					throws SQLException {
 				AclSnapshot snapshot = batch.get(i);
 				ps.setLong(1, snapshot.getTimestamp());
-				ps.setLong(2, snapshot.getOwnerId());
+				ps.setLong(2, Long.parseLong(snapshot.getId()));
 				ps.setString(3, snapshot.getOwnerType().name());
-				if (snapshot.getCreatedOn() != null) {
-					ps.setLong(4, snapshot.getCreatedOn());
+				if (snapshot.getCreationDate() != null) {
+					ps.setLong(4, snapshot.getCreationDate().getTime());
 				} else {
 					ps.setNull(4, Types.BIGINT);
+				}
+				if (snapshot.getResourceAccess() != null) {
+					String xml = XMLUtils.toXML(snapshot.getResourceAccess(), RESOURCE_ACCESS_ALIAS);
+					Blob blob = template.getDataSource().getConnection().createBlob();
+					blob.setBytes(1, xml.getBytes());
+					ps.setBlob(5, blob);
+				} else {
+					ps.setNull(5, Types.BLOB);
 				}
 			}
 		});
--- src/test/java/org/sagebionetworks/warehouse/workers/db/AclSnapshotDaoImplTest.java
+++ src/test/java/org/sagebionetworks/warehouse/workers/db/AclSnapshotDaoImplTest.java
@@ -3,12 +3,15 @@ package org.sagebionetworks.warehouse.workers.db;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.sagebionetworks.repo.model.ObjectType;
+import org.sagebionetworks.repo.model.ResourceAccess;
 import org.sagebionetworks.warehouse.workers.model.AclSnapshot;
 import org.sagebionetworks.warehouse.workers.utils.ObjectSnapshotTestUtil;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -30,17 +33,24 @@ public class AclSnapshotDaoImplTest {
 	public void test() {
 		AclSnapshot snapshot1 = ObjectSnapshotTestUtil.createValidAclSnapshot();
 		AclSnapshot snapshot2 = ObjectSnapshotTestUtil.createValidAclSnapshot();
-
-		dao.insert(Arrays.asList(snapshot1, snapshot2));
-		assertEquals(snapshot1, dao.get(snapshot1.getTimestamp(), snapshot1.getOwnerId(), snapshot1.getOwnerType()));
-		assertEquals(snapshot2, dao.get(snapshot2.getTimestamp(), snapshot2.getOwnerId(), snapshot2.getOwnerType()));
-
+		snapshot2.setResourceAccess(new HashSet<ResourceAccess>());
 		AclSnapshot snapshot3 = ObjectSnapshotTestUtil.createValidAclSnapshot();
-		snapshot3.setTimestamp(snapshot2.getTimestamp());
-		snapshot3.setOwnerId(snapshot2.getOwnerId());
-		snapshot3.setOwnerType(snapshot2.getOwnerType());
-		dao.insert(Arrays.asList(snapshot3));
-		assertEquals(snapshot2, dao.get(snapshot3.getTimestamp(), snapshot3.getOwnerId(), snapshot3.getOwnerType()));
+		ResourceAccess ra = new ResourceAccess();
+		Set<ResourceAccess> set = new HashSet<ResourceAccess>();
+		set.add(ra);
+		snapshot3.setResourceAccess(set);
+
+		dao.insert(Arrays.asList(snapshot1, snapshot2, snapshot3));
+		assertEquals(snapshot1, dao.get(snapshot1.getTimestamp(), Long.parseLong(snapshot1.getId()), snapshot1.getOwnerType()));
+		assertEquals(snapshot2, dao.get(snapshot2.getTimestamp(), Long.parseLong(snapshot2.getId()), snapshot2.getOwnerType()));
+		assertEquals(snapshot3, dao.get(snapshot3.getTimestamp(), Long.parseLong(snapshot3.getId()), snapshot3.getOwnerType()));
+
+		AclSnapshot snapshot4 = ObjectSnapshotTestUtil.createValidAclSnapshot();
+		snapshot4.setTimestamp(snapshot2.getTimestamp());
+		snapshot4.setId(snapshot2.getId());
+		snapshot4.setOwnerType(snapshot2.getOwnerType());
+		dao.insert(Arrays.asList(snapshot4));
+		assertEquals(snapshot2, dao.get(snapshot4.getTimestamp(), Long.parseLong(snapshot4.getId()), snapshot4.getOwnerType()));
 	}
 
 	@Test (expected=EmptyResultDataAccessException.class)
