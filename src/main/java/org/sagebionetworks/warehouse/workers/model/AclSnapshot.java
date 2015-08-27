0df302bc4cb635686016878db8bec11c0460ee5b
Kimyen <kimyen0816@gmail.com>
2015-08-26 17:04:49 -0700

    extract the resource access alias and put it in the model object

--- src/main/java/org/sagebionetworks/warehouse/workers/model/AclSnapshot.java
+++ src/main/java/org/sagebionetworks/warehouse/workers/model/AclSnapshot.java
@@ -6,6 +6,7 @@ public class AclSnapshot extends AclRecord {
 
 	private static final long serialVersionUID = 1L;
 	Long timestamp;
+	public static final String RESOURCE_ACCESS_ALIAS = "ResourceAccessSet";
 
 	public Long getTimestamp() {
 		return timestamp;
