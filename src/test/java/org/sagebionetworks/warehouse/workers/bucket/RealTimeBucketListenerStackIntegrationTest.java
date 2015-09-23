74e461ff0483bb6e40b25790b3f52a6f1ff0ee25
Kimyen <kimyen0816@gmail.com>
2015-09-23 10:48:36 -0700

    clean up

--- src/test/java/org/sagebionetworks/warehouse/workers/bucket/RealTimeBucketListenerStackIntegrationTest.java
+++ src/test/java/org/sagebionetworks/warehouse/workers/bucket/RealTimeBucketListenerStackIntegrationTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.sagebionetworks.aws.utils.s3.BucketDao;
 import org.sagebionetworks.aws.utils.s3.BucketDaoImpl;
