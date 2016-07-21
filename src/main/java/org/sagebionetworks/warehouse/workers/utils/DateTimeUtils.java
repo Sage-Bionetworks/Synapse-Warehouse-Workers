55e43ebc8ff8ece3c9f977b29c296a2eb9a6f3a8
Kimyen <kimyen0816@gmail.com>
2016-07-21 16:27:22 -0700

    add supporting method to DateTimeUtils

--- src/main/java/org/sagebionetworks/warehouse/workers/utils/DateTimeUtils.java
+++ src/main/java/org/sagebionetworks/warehouse/workers/utils/DateTimeUtils.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.joda.time.DateTime;
 
 public class DateTimeUtils {
+	public static final String DATE_FORMAT = "%04d-%02d-%02d";
 
 	/**
 	 * This method takes a java.util.Date and return the next month java.util.Date
@@ -15,5 +16,15 @@ public class DateTimeUtils {
 	public static Date getNextMonth(Date month) {
 		DateTime time = new DateTime(month).plusMonths(1);
 		return time.toDate();
+	}
+
+	/**
+	 * 
+	 * @param date
+	 * @return a string representation of date in format yyyy-mm-dd
+	 */
+	public static String toDateString(Date date) {
+		DateTime time = new DateTime(date);
+		return String.format(DATE_FORMAT, time.getYear(), time.getMonthOfYear(), time.getDayOfMonth());
 	}
 }
--- src/test/java/org/sagebionetworks/warehouse/workers/utils/DateTimeUtilsTest.java
+++ src/test/java/org/sagebionetworks/warehouse/workers/utils/DateTimeUtilsTest.java
@@ -4,19 +4,26 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class DateTimeUtilsTest {
 
 	@Test
 	public void testGetNextMonthSameYear(){
-		Date date = new Date(2016, 1, 1);
-		assertEquals(new Date(2016, 2, 1), DateTimeUtils.getNextMonth(date));
+		Date date = new DateTime(2016, 1, 1, 0, 0).toDate();
+		assertEquals(new DateTime(2016, 2, 1, 0, 0).toDate(), DateTimeUtils.getNextMonth(date));
 	}
 
 	@Test
 	public void testGetNextMonthDifferentYear(){
-		Date date = new Date(2015, 12, 1);
-		assertEquals(new Date(2016, 1, 1), DateTimeUtils.getNextMonth(date));
+		Date date = new DateTime(2015, 12, 1, 0, 0).toDate();
+		assertEquals(new DateTime(2016, 1, 1, 0, 0).toDate(), DateTimeUtils.getNextMonth(date));
+	}
+
+	@Test
+	public void testToDateString(){
+		Date date = new DateTime(2016, 1, 14, 0, 0).toDate();
+		assertEquals("2016-01-14", DateTimeUtils.toDateString(date));
 	}
 }
